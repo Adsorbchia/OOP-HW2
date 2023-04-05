44376ff22906efaa2be57444b70aa917932f65b4
Natalya <117934761+Adsorbchia@users.noreply.github.com>
2023-04-06 00:18:27 +0400

    feat:add new laptop

---
+++ Honor.java
@@ -0,0 +1,14 @@
+package org.ex.seminar2.hw2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Honor extends Laptop {
+    public Honor(String color, double screenDiagonal, int screenRefreshRate, String screenResolution, String processor, int ram, String driveType, String manufacturer, double price) {
+        super(color, screenDiagonal, screenRefreshRate, screenResolution, processor, ram, driveType, manufacturer, price);
+    }
+
+
+    }
+
+
