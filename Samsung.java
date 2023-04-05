c4226c835f7ff04166844713740b026f64abca66
Natalya <117934761+Adsorbchia@users.noreply.github.com>
2023-04-06 00:26:13 +0400

    feat:add new class

---
+++ Samsung.java
@@ -0,0 +1,9 @@
+package org.ex.seminar2.hw2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Samsung extends Laptop{
+    public Samsung(String color, double screenDiagonal, int screenRefreshRate, String screenResolution, String processor, int ram, String driveType, String manufacturer, double price) {
+        super(color, screenDiagonal, screenRefreshRate, screenResolution, processor, ram, driveType, manufacturer, price);
+    }}
